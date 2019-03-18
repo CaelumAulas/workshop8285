4cb8c20e62ca436d0da6a6f8b2a0d35a89674718
Artur Diniz Adam <artur.daz@gmail.com>
2019-03-18 18:23:04 -0300

    Final da aula

--- js/dialog.js
+++ js/dialog.js
@@ -4,15 +4,27 @@ var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+var ignore = document.querySelectorAll('main, header, footer')
+
+btnAbreDialog.style.display = 'block'
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
+  ignore.forEach(el => el.setAttribute('inert','true'))
+
   dialog.classList.add('dialogNewsletter--aberto');
+  dialogBody.querySelector('.dialogNewsletter-campo').focus()
 });
 
 function fechandoDialog() {
-  document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  ignore.forEach(el => el.removeAttribute('inert'))
+
+  dialog.classList.remove('dialogNewsletter--aberto');
+  document.activeElement.blur()
+
+  setTimeout(() => {
+    btnAbreDialog.focus()
+  }, 0)
 }
 
 // Listeners
